43a3b507b53562ba81d73add15fd84dabc98a2d5
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2022-03-15 18:22:02 -0500

    Begin home page

---
+++ Shooping/Shooping/Models/ProductsHomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Shooping.Data.Entities;
+
+namespace Shooping.Models
+{
+    public class ProductsHomeViewModel
+    {
+        public Product Product1 { get; set; }
+
+        public Product Product2 { get; set; }
+
+        public Product Product3 { get; set; }
+
+        public Product Product4 { get; set; }
+    }
+}
