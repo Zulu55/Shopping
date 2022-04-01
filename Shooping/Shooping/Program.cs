189b08f98440f0652161005a2d31fb2c65f8ccee
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2022-04-01 06:30:34 -0500

    Add icons

--- Shooping/Shooping/Program.cs
+++ Shooping/Shooping/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddIdentity<User, IdentityRole>(cfg =>
     cfg.Password.RequireLowercase = false;
     cfg.Password.RequireNonAlphanumeric = false;
     cfg.Password.RequireUppercase = false;
-    cfg.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    cfg.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(30);
     cfg.Lockout.MaxFailedAccessAttempts = 3;
     cfg.Lockout.AllowedForNewUsers = true;
 })
