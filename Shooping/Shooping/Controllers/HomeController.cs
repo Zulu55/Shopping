84dbdf8cf53e4f1a0de72c44230b88fa56c9577c
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2022-04-19 20:56:07 -0500

    Basic home screen

--- Shooping/Shooping/Controllers/HomeController.cs
+++ Shooping/Shooping/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shooping.Data;
+using Shooping.Data.Entities;
 using Shooping.Models;
 using System.Diagnostics;
 
@@ -7,15 +10,48 @@ namespace Shooping.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Product>? products = await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .OrderBy(p => p.Description)
+                .ToListAsync();
+
+            List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
+            int i = 1;
+            foreach (Product? product in products)
+            {
+                if (i == 1)
+                {
+                    productsHome.LastOrDefault().Product1 = product;
+                }
+                if (i == 2)
+                {
+                    productsHome.LastOrDefault().Product2 = product;
+                }
+                if (i == 3)
+                {
+                    productsHome.LastOrDefault().Product3 = product;
+                }
+                if (i == 4)
+                {
+                    productsHome.LastOrDefault().Product4 = product;
+                    productsHome.Add(new ProductsHomeViewModel());
+                    i = 0;
+                }
+                i++;
+            }
+
+            return View(productsHome);
         }
 
         public IActionResult Privacy()
