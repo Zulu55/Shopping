d8abdcac78ba5400f5de7c858408ac87c39ab26b
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2022-04-27 06:36:24 -0500

    Add detail in home

---
+++ Shooping/Shooping/Models/AddProductToCartViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Shooping.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shooping.Models
+{
+    public class AddProductToCartViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Descripción")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal Price { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        [Display(Name = "Inventario")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public float Stock { get; set; }
+
+        [Display(Name = "Categorías")]
+        public string Categories { get; set; }
+
+        public ICollection<ProductImage> ProductImages { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        [Display(Name = "Cantidad")]
+        [Range(0.0000001, float.MaxValue, ErrorMessage = "Debes de ingresar un valor mayor a cero en la cantidad.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public float Quantity { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Comentarios")]
+        public string Remarks { get; set; }
+    }
+}
