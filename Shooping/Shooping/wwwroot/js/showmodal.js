7781a275f668d376786c6448d3ab37053b053149
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2022-05-03 20:56:16 -0500

    Generic modal changes and spinner

---
+++ Shooping/Shooping/wwwroot/js/showmodal.js
@@ -0,0 +1,94 @@
+﻿showInPopup = (url, title) => {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (res) {
+            $('#form-modal .modal-body').html(res);
+            $('#form-modal .modal-title').html(title);
+            $('#form-modal').modal('show');
+
+        }
+    })
+}
+
+jQueryAjaxPost = form => {
+    try {
+        $.ajax({
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.isValid) {
+                    $('#view-all').html(res.html)
+                    $('#form-modal .modal-body').html('');
+                    $('#form-modal .modal-title').html('');
+                    $('#form-modal').modal('hide');
+                    // reload the table         
+                    location.reload()
+
+                }
+                else
+
+                    $('#form-modal .modal-body').html(res.html);
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        })
+        //to prevent default form submit event
+        return false;
+    } catch (ex) {
+        console.log(ex)
+    }
+}
+
+(function (soccerDeleteDialog) {
+
+    var methods = {
+        "openModal": openModal,
+        "deleteItem": deleteItem
+    };
+
+    var item_to_delete;
+
+    /**
+         * Open a modal by class name or Id.
+         *
+         * @return string id item.
+         */
+    function openModal(modalName, classOrId, sourceEvent, deletePath, eventClassOrId) {
+        var textEvent;
+        if (classOrId) {
+            textEvent = "." + modalName;
+        } else {
+            textEvent = "#" + modalName;
+        }
+
+        $(textEvent).click((e) => {
+            item_to_delete = e.currentTarget.dataset.id;
+            deleteItem(sourceEvent, deletePath, eventClassOrId);
+        });
+    }
+
+    /**
+     * Path to delete an item.
+     *
+     * @return void.
+     */
+    function deleteItem(sourceEvent, deletePath, eventClassOrId) {
+        var textEvent;
+        if (eventClassOrId) {
+            textEvent = "." + sourceEvent;
+        } else {
+            textEvent = "#" + sourceEvent;
+        }
+        $(textEvent).click(function () {
+            window.location.href = deletePath + item_to_delete;
+        });
+    }
+
+    soccerDeleteDialog.sc_deleteDialog = methods;
+
+})(window);
