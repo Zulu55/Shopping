3d46822a26f4fd1713599e76c06ba7210a65a354
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2022-05-10 17:56:52 -0500

    Add Dashboard

---
+++ Shooping/Shooping/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shooping.Data;
+using Shooping.Enums;
+using Shooping.Helpers;
+
+namespace Shooping.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+
+        public DashboardController(DataContext context, IUserHelper userHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.UsersCount = _context.Users.Count();
+            ViewBag.ProductsCount = _context.Products.Count();
+            ViewBag.NewOrdersCount = _context.Sales.Where(o => o.OrderStatus == OrderStatus.Nuevo).Count();
+            ViewBag.ConfirmedOrdersCount = _context.Sales.Where(o => o.OrderStatus == OrderStatus.Confirmado).Count();
+
+            return View(await _context.TemporalSales
+                    .Include(u => u.User)
+                    .Include(p => p.Product).ToListAsync());
+        }
+    }
+}
